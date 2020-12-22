4a358d4a73f4c1bc9db11ca7c451e2a27870dd66
szx666 <1193131944@qq.com>
2020-12-22 11:22:01 +0800

    上传dockerfile——springboot项目进行测试

---
+++ src/main/java/com/example/DockerHelloworld1221Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerHelloworld1221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerHelloworld1221Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/web/controller.java
@@ -0,0 +1,14 @@
+package com.example.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello,world!";
+    }
+}
